6285ce10ff5164b4fa82734be348d55c2a19a73f
Mohammad Yassen <mohammadgithub9989@gmail.com>
2025-02-17 15:42:36 +0300

    inital commit

---
+++ database/migrations/2025_02_17_111639_create_posts_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('slug');
+            $table->text('body');
+            $table->boolean('is_pinned')->default(false);
+            $table->foreignId('user_id')->constrained('users')->cascadeOnDelete();
+            $table->foreignId('corner_id')->nullable()->constrained('corners')->cascadeOnDelete();
+            $table->enum('privacy', ['public', 'private','friendsOnly'])->default('friendsOnly');
+            $table->fullText('slug');
+            $table->index('text');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('posts');
+    }
+};
---
+++ database/migrations/2025_02_17_112243_create_corner_user_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('corner_user', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users')->cascadeOnDelete();
+            $table->foreignId('corner_id')->constrained('corners')->cascadeOnDelete();
+            $table->enum('type',['Newbie','Admin','Contributor','Owner'])->default('Newbie');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('admin_corner');
+    }
+};
